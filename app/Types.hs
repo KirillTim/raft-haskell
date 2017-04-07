36e8828a280de01fd6dc3a629b189a42def868b0
Kirill <timofeev.kirill.a@gmail.com>
2017-04-07 11:48:25 +0300

    state init

--- app/Types.hs
+++ app/Types.hs
@@ -1,21 +1,30 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Types where
 
-import Data.Map
+import qualified Data.Map as M
 
 import Data.Binary (Binary)
 import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
 
+import Control.Monad.RWS.Strict --(RWS, MonadReader, MonadWriter, MonadState,
+                                -- ask, tell, get, put, execRWS, liftIO)
+
 import Control.Lens (makeLenses)
 
 data Role = Candidate | Follower | Leader deriving (Show, Eq)
 
-newtype Term = Term Int deriving (Show, Eq, Ord, Generic, Typeable)
+newtype Term = Term Int deriving (Show, Eq, Ord, Num, Generic, Typeable)
 newtype LogIndex = LogIndex Int deriving (Show, Eq, Ord, Generic, Typeable)
+
+initTerm = Term 0
+initIndex = LogIndex (-1)
+
 
 data LogEntry = LogEntry
   { _index :: LogIndex,
@@ -36,7 +45,7 @@ data Config = Config
     _others :: [NodeInfo]
   } deriving (Show, Eq)
 
-data NodeName = String
+data NodeName = String deriving (Show, Generic, Typeable)
 
 data NodeState = NodeState
   { _role               :: Role,
@@ -46,35 +55,101 @@ data NodeState = NodeState
     _log                :: [LogEntry],
     _commitIndex        :: LogIndex,
     _lastApplied        :: LogIndex,
-    _currentLeader      :: NodeInfo,
+    _currentLeader      :: Maybe NodeInfo,
     _votesForMe         :: Int,
-    _nextIndex          :: Map NodeName LogIndex,
-    _matchIndex         :: Map NodeName LogIndex,
-    _storage            :: Map String String
-  }
+    _nextIndex          :: M.Map NodeName LogIndex,
+    _matchIndex         :: M.Map NodeName LogIndex,
+    _storage            :: M.Map String String
+  } deriving (Show)
 
 makeLenses ''NodeState
+
+testNodes :: Int -> [NodeInfo]
+testNodes n = fmap (\x -> NodeInfo ("node"++show x) "localhost" (9000+x) True) [1..n]
+
+initTestConfig :: Config
+initTestConfig = Config
+  (head $ testNodes 3)
+  (tail $ testNodes 3)
+
+initNodeState :: NodeState
+initNodeState = NodeState
+  Follower
+  initTestConfig
+  initTerm
+  Nothing
+  []
+  initIndex
+  initIndex
+  Nothing
+  0
+  M.empty
+  M.empty
+  M.empty
+
 --messages
-data AppendEntries = AppendEntries
+data Message =
+  AppendEntries
   { _term    :: Term,
     _leaderName    :: String,
     _prevLogIndex  :: LogIndex,
     _prevLogTerm   :: Term,
     _entries       :: [LogEntry],
     _leaderCommit  :: LogIndex
-  } deriving (Show, Generic, Typeable)
-
-data AppendRejected = AppendRejected { _term :: Term } deriving (Show, Generic, Typeable)
-
-data AppendSuccessfull = AppendSuccessfull deriving (Show, Generic, Typeable)
-
-data RequestVote = RequestVote
+  }
+  | AppendRejected
+  { _nodeName :: String,
+    _term     :: Term
+  }
+  | AppendSuccessfull
+  { _nodeName  :: String,
+    _term      :: Term,
+    _lastIndex :: LogIndex
+  }
+  | RequestVote
   { _term :: Term,
     _candidateName :: String,
     _lastLogIndex  :: LogIndex,
     _lastLogTerm   :: Term
+  }
+  | VoteForCandidate { _term :: Term }
+  | DeclineCandidate { _term :: Term }
+  | ElectionTimeout
+  | HeartbeatTimeout deriving (Show, Generic, Typeable)
+
+data AddressedMessage = AddressedMessage
+  { recipient :: NodeName,
+    message   :: Message
   } deriving (Show, Generic, Typeable)
 
-data VoteForCandidate = VoteForCandidate { _term :: Term } deriving (Show, Generic, Typeable)
+newtype NodeAction a = NodeAction {runAction :: RWST Config [AddressedMessage] NodeState IO a}
+    deriving (Functor, Applicative, Monad, MonadState NodeState,
+              MonadWriter [AddressedMessage], MonadReader Config)
 
-data DeclineCandidate = VoteForCandidate { _term :: Term } deriving (Show, Generic, Typeable)
+data LittleCfg = LittleCfg { _ignore :: [Int]} deriving (Show)
+data LittleState = LittleState { _next :: Int } deriving (Show)
+
+comp :: Int -> RWST LittleCfg [String] LittleState IO ()
+comp start = do
+  r <- ask
+  let add = case start `elem` _ignore r of True -> 1
+                                           False -> start
+  tell ["start is " ++ show start]
+  tell ["add is " ++ show add]
+  liftIO $ putStrLn $ show add
+  prev <- get
+  let new = prev { _next = _next prev + add}
+  put new
+  tell ["new is " ++ show new]
+  
+{-comp :: Int -> RWST [Int] [Int] Int IO ()
+comp start = do
+  r <- ask
+  let add = case start `elem` r of True  -> 1
+                                   False -> start
+  liftIO $ putStrLn $ show add
+  prev <- get
+  let new = prev + add
+  put new
+  tell [start]
+  if new < 100 then runRWST (comp new) r prev else return () -}
