cfe4deaeb7b72d64cd0f6868370a943035d89491
Kirill <timofeev.kirill.a@gmail.com>
2017-04-10 17:15:23 +0300

    fix test warnings and imports

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,18 @@
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
-import           Control.Lens             (set, (&), (.=), (.~), (^.))
-import           Control.Monad.RWS.Strict
+import           Control.Lens             ((&), (.~), (^.))
+import           Control.Monad.RWS.Strict (runRWST)
 import           Data.List                (elemIndex)
-import           Handlers
-import           Types
+import           Handlers                 (handleElectionTimeout,
+                                           handleHeartBeatTimeout,
+                                           handleRequestVote)
+import           Types                    (Command (..), Config (..),
+                                           LogEntry (..), Message (..),
+                                           MessageTo (..), NodeInfo (..),
+                                           NodeState (..), Role (..), Term (..),
+                                           currentTerm, eLog, initNodeState,
+                                           role, votedForOnThisTerm)
 
 testNodes :: Int -> [NodeInfo]
 testNodes n = fmap (\x -> NodeInfo ("node"++show x) "localhost" (show $ 9000+x) True) [1..n]
@@ -18,23 +25,26 @@ initTestConfig = Config
   (head $ testNodes 3)
   (tail $ testNodes 3)
 
+initTestNodeState :: NodeState
 initTestNodeState = initNodeState
-
+testFollowerHandleElectionTimeout :: TestTree
 testFollowerHandleElectionTimeout = testCase "election timeout" $ do
-  let init = initTestNodeState
-  (_, st, msgs) <- runRWST handleElectionTimeout initTestConfig init
+  let initSt = initTestNodeState
+  (_, st, msgs) <- runRWST handleElectionTimeout initTestConfig initSt
   assertBool "restart timer" $ elemIndex StopElectionTimeout msgs < elemIndex StartElectionTimeout msgs
   assertBool "request vote from node2" $ "node2" `elem` fmap to msgs
   assertBool "request vote from node3" $ "node3" `elem` fmap to msgs
   assertEqual "new state is Candidate" (st^.role) Candidate
   assertBool "increment term" $ st^.currentTerm == initNodeState^.currentTerm + 1
 
+testFollowerHandleHeartBeatTimeout :: TestTree
 testFollowerHandleHeartBeatTimeout = testCase "ignore heartbeat timeout" $ do
-  let init = initTestNodeState
-  (_, st, msgs) <- runRWST handleHeartBeatTimeout initTestConfig init
-  assertEqual "same state" st init
+  let initSt = initTestNodeState
+  (_, st, msgs) <- runRWST handleHeartBeatTimeout initTestConfig initSt
+  assertEqual "same state" st initSt
   assertBool "no response" $ null msgs
 
+testFollowerHandleRequestVote :: TestTree
 testFollowerHandleRequestVote = testCase "request vote" $ do -- TODO: split it
   let st = initTestNodeState & currentTerm .~ (Term 42) & eLog .~ testLogT42Len2
   let rvSmallTerm = RequestVote 20 "node2" 3 20
@@ -42,18 +52,20 @@ testFollowerHandleRequestVote = testCase "request vote" $ do -- TODO: split it
   assertEqual "decline" [MessageToNode "node2" $ DeclineCandidate 42] msgs
   assertEqual "same state" st st1
   let rvOK = RequestVote 42 "node2" 3 42
-  (_, st2, msgs) <- runRWST (handleRequestVote "node2" rvOK) initTestConfig st1
-  assertBool "accept" $ (MessageToNode "node2" $ VoteForCandidate 42) `elem` msgs
-  assertBool "restart timer" $ elemIndex StopElectionTimeout msgs < elemIndex StartElectionTimeout msgs
+  (_, st2, msgs1) <- runRWST (handleRequestVote "node2" rvOK) initTestConfig st1
+  assertBool "accept" $ (MessageToNode "node2" $ VoteForCandidate 42) `elem` msgs1
+  assertBool "restart timer" $ elemIndex StopElectionTimeout msgs < elemIndex StartElectionTimeout msgs1
   assertEqual "votedFor" (st2^.votedForOnThisTerm) (Just "node2")
   let rvVotedThisTerm = RequestVote 42 "node3" 3 42
-  (_, st3, msgs) <- runRWST (handleRequestVote "node3" rvVotedThisTerm) initTestConfig st2
-  assertEqual "rejected" [MessageToNode "node3" $ DeclineCandidate 42] msgs
+  (_, st3, msgs2) <- runRWST (handleRequestVote "node3" rvVotedThisTerm) initTestConfig st2
+  assertEqual "rejected" [MessageToNode "node3" $ DeclineCandidate 42] msgs2
   assertEqual "same state" st2 st3 -- TODO: custom asserts?
 
-testFollowerHandleAppendEntries = testCase "append entries" $ do
-  undefined
+-- testFollowerHandleAppendEntries :: TestTree
+-- testFollowerHandleAppendEntries = testCase "append entries" $ do
+--   undefined
 
+followerTestGroup :: TestTree
 followerTestGroup = testGroup "Follower state tests" [testFollowerHandleElectionTimeout,
                                                        testFollowerHandleHeartBeatTimeout,
                                                        testFollowerHandleRequestVote
